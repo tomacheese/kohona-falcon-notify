90a870a00954eed0492a16d1ec2a6ec70af7ac3a
Tomachi <8929706+book000@users.noreply.github.com>
2024-06-02 11:29:14 +0900

    feat: v0 (#1)
    * feat: v0
    
    * fix: add dom lib

---
+++ src/config.ts
@@ -0,0 +1,42 @@
+import { ConfigFramework } from '@book000/node-utils'
+
+export interface Configuration {
+  twitter: {
+    username: string
+    password: string
+    otpSecret?: string
+  }
+  discord: {
+    token: string
+    channelId: string
+  }
+}
+export class SDNConfiguration extends ConfigFramework<Configuration> {
+  protected validates(): Record<string, (config: Configuration) => boolean> {
+    return {
+      'config is defined': (config) => !!config,
+      'twitter is defined': (config) => !!config.twitter,
+      'twitter.username is defined': (config) => !!config.twitter.username,
+      'twitter.username is string': (config) =>
+        typeof config.twitter.username === 'string',
+      'twitter.username is not empty': (config) =>
+        config.twitter.username.trim() !== '',
+      'twitter.password is defined': (config) => !!config.twitter.password,
+      'twitter.password is string': (config) =>
+        typeof config.twitter.password === 'string',
+      'twitter.password is not empty': (config) =>
+        config.twitter.password.trim() !== '',
+      'discord is defined': (config) => !!config.discord,
+      'discord.token is defined': (config) => !!config.discord.token,
+      'discord.token is string': (config) =>
+        typeof config.discord.token === 'string',
+      'discord.token is not empty': (config) =>
+        config.discord.token.trim() !== '',
+      'discord.channelId is defined': (config) => !!config.discord.channelId,
+      'discord.channelId is string': (config) =>
+        typeof config.discord.channelId === 'string',
+      'discord.channelId is not empty': (config) =>
+        config.discord.channelId.trim() !== '',
+    }
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,129 @@
+import { Discord, DiscordEmbed, Logger } from '@book000/node-utils'
+import { SDNConfiguration as KFNConfiguration } from './config'
+import { Notified } from './notified'
+import { FullUser } from 'twitter-d'
+import { Twitter } from '@book000/twitterts'
+
+function isFullUser(user: any): user is FullUser {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  return user.id_str !== undefined
+}
+
+async function main() {
+  const logger = Logger.configure('main')
+  logger.info('✨ main()')
+  const config = new KFNConfiguration()
+  config.load()
+  if (!config.validate()) {
+    logger.error('❌ Config is invalid')
+    for (const failure of config.getValidateFailures()) {
+      logger.error('- ' + failure)
+    }
+    return
+  }
+  logger.info('✅ Config is valid. Login to Twitter...')
+
+  const twitter = await Twitter.login({
+    username: config.get('twitter').username,
+    password: config.get('twitter').password,
+    otpSecret: config.get('twitter').otpSecret,
+    puppeteerOptions: {
+      executablePath: process.env.CHROMIUM_PATH,
+      userDataDirectory: process.env.USER_DATA_DIRECTORY ?? './data/userdata',
+    },
+    debugOptions: {
+      outputResponse: {
+        enable: true,
+      },
+    },
+  })
+  try {
+    const discordConfig = config.get('discord')
+    const discord = new Discord({
+      token: discordConfig.token,
+      channelId: discordConfig.channelId,
+    })
+
+    // 1. Get the latest 200 tweets of a specific user using the `statuses/user_timeline` API.
+    logger.info('🔍 Fetching tweets...')
+    const tweets = await twitter.getUserTweets({
+      screenName: 'kohona_poke',
+      limit: 200,
+    })
+    logger.info(`🔍 Fetched ${tweets.length} tweets`)
+
+    const notified = new Notified(
+      process.env.NOTIFIED_PATH ?? './data/notified.json'
+    )
+
+    // 2. When operating for the first time (= initialize mode), save the tweet ID of the acquired tweets as notified.
+    const initializeMode = notified.isFirst()
+    if (initializeMode) {
+      logger.info('💾 Initialize mode. Save all tweets to file')
+      for (const tweet of tweets) {
+        notified.add(tweet.id_str)
+      }
+
+      logger.info('🚀 Closing browser...')
+      await twitter.close()
+      return
+    }
+
+    // 3.
+    // ツイート内容が「ファルコン・(1文字以上～10文字以内の文字列)」かつ「画像」
+    // ツイート内容が「2戦目」かつ「画像」
+    const notifyTweets = tweets.filter((tweet) => {
+      return (
+        !notified.isNotified(tweet.id_str) &&
+        (tweet.full_text.match(/ファルコン・.{1,10}/) !== null ||
+          tweet.full_text.includes('2戦目'))
+      )
+    })
+    logger.info(`🔔 Notify ${notifyTweets.length} tweets`)
+
+    // 4. Post filtered tweets to Discord. The tweet ID of the posted tweets will be saved as notified.
+    for (const tweet of notifyTweets.reverse()) {
+      if (!isFullUser(tweet.user)) {
+        continue
+      }
+      if (!tweet.entities.media) {
+        continue
+      }
+
+      const tweetUrl = `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+      const imageUrl = tweet.entities.media[0].media_url_https
+
+      logger.info(`Send message to Discord: ${tweetUrl}`)
+      const embed: DiscordEmbed = {
+        title: 'ファルコン・コホナ',
+        description: tweetUrl,
+        image: {
+          url: imageUrl,
+        },
+        color: 0x00_ff_00,
+        timestamp: new Date(tweet.created_at).toISOString(),
+      }
+      await discord.sendMessage({
+        content: '',
+        embeds: [embed],
+      })
+      notified.add(tweet.id_str)
+
+      // wait 1 second (Discord API rate limit)
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+    }
+  } catch (error) {
+    logger.error("Error: Couldn't fetch tweets", error as Error)
+  }
+
+  logger.info('🚀 Closing browser...')
+  await twitter.close()
+}
+
+;(async () => {
+  await main().catch((error: unknown) => {
+    Logger.configure('main').error('❌ Error', error as Error)
+    // eslint-disable-next-line unicorn/no-process-exit
+    process.exit(1)
+  })
+})()
